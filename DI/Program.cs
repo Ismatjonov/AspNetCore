65b2dc7d726e0be4e0ebf10fa65e9768ca5cacb8
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-07-29 16:23:55 +0500

    Dependency Injection

---
+++ DI/Program.cs
@@ -0,0 +1,27 @@
+using System.Text;
+using Microsoft.Extensions.Primitives;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var app = builder.Build();
+
+app.Run(async (context) =>
+{
+    var sb = new StringBuilder();
+    sb.Append("<h1>All Services</h1>");
+    sb.Append("<table border='1' cellpadding='0' cellspacing='0'>");
+    sb.Append("<tr><th>Type</th><th>Lifetime</th><th>Realization</th></tr>");
+    foreach (var svc in services)
+    {
+        sb.Append("<tr>");
+        sb.Append($"<td>{svc.ServiceType.FullName}</td>");
+        sb.Append($"<td>{svc.Lifetime}</td>");
+        sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
+        sb.Append("</tr>");
+    }
+    sb.Append("</table>");
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.WriteAsync(sb.ToString());
+});
+
+app.Run();
