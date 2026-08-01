30e9628e9628169743f82cb88ef91b61f0bf68f6
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-08-01 23:45:12 +0500

    Passing dependency with constructor of class

--- DI/TimeServiceMiddleware.cs
+++ DI/TimeServiceMiddleware.cs
@@ -2,14 +2,16 @@ namespace DI;
 
 public class TimeServiceMiddleware
 {
-    readonly RequestDelegate next;
+    RequestDelegate next;
+    ITimeService timeService;
 
-    public TimeServiceMiddleware(RequestDelegate next)
+    public TimeServiceMiddleware(RequestDelegate next, ITimeService timeService)
     {
         this.next = next;
+        this.timeService = timeService;
     }
 
-    public async Task InvokeAsync(HttpContext context, ITimeService timeService)
+    public async Task InvokeAsync(HttpContext context)
     {
         context.Response.ContentType = "text/html; charset=utf-8";
         await context.Response.WriteAsync($"<h1>Time: {timeService?.GetTime()}</h1>");
