6bfe96c880edb1d97d7511c0cc898a0bb5c79be5
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-08-11 15:27:03 +0500

    Route parameters

--- RoutingApp/Program.cs
+++ RoutingApp/Program.cs
@@ -14,24 +14,24 @@ app.Map("/contact", () => "Contact Page");
 // app.Map("/user", () => new Person("Bakhtovar", 20));
 
 // doing nothing, just logging some message in console
-app.Map("/log", () => Console.WriteLine("Request Path: /log"));
+// app.Map("/log", () => Console.WriteLine("Request Path: /log"));
 
 // Putting route handler into the method
-app.Map("/", IndexHandler);
-app.Map("/user", UserHandler);
+/*app.Map("/", IndexHandler);
+app.Map("/user", UserHandler);*/
 
 // another version of method with RequestDelegate
-app.Map("/about", async context =>
+/*app.Map("/about", async context =>
 {
     await context.Response.WriteAsync("About Page!");
-});
+});*/
 
 // ---------- Getting all routes of app ----------
 /*app.MapGet("/routes", (IEnumerable<EndpointDataSource> endpointDataSource) =>
     string.Join("\n", endpointDataSource.SelectMany(source => source.Endpoints)));*/
 
 // getting detailed information
-app.Map("/routes", (IEnumerable<EndpointDataSource> endpointsDataSource) =>
+/*app.Map("/routes", (IEnumerable<EndpointDataSource> endpointsDataSource) =>
 {
     var sb = new StringBuilder();
     var endpoints = endpointsDataSource.SelectMany(es => es.Endpoints);
@@ -46,6 +46,14 @@ app.Map("/routes", (IEnumerable<EndpointDataSource> endpointsDataSource) =>
 
     return sb.ToString();
 });
+app.Run();*/
+
+
+// ==================== Route Parameters ====================
+app.Map("/users/{id}", (string id) => $"User Id: {id}");
+app.Map("/users", () => "Users Page");
+app.Map("/", () => "Index Page");
+
 app.Run();
 
 string IndexHandler()
--- RoutingApp/obj/Debug/net10.0/RoutingApp.AssemblyInfo.cs
+++ RoutingApp/obj/Debug/net10.0/RoutingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoutingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4806331c8c84d6a1592db2d24f970df3d1838f51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e18e5bcfaaf4021f5e20ec7997d60c4790c930e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoutingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoutingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
